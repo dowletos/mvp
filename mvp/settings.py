3e626e39e3ec777ada80256805f5ae29de12d873
dowletos <dkomekow@gmail.com>
2023-01-30 17:53:19 +0500

    L2

--- mvp/settings.py
+++ mvp/settings.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'news'
+    'news',
+    'mvp'
 ]
 
 MIDDLEWARE = [
--- mvp/urls.py
+++ mvp/urls.py
@@ -17,13 +17,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from news.views import index
+from .views import mainpage
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('news/', index)
+    path('news/', index),
+    path('',mainpage)
 ]
 
 if settings.DEBUG:
